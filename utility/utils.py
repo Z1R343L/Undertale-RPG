77f3a46b331335ec6edafb36739a4971f962469f
LetsChill <LetsXhill@gmail.com>
2022-05-07 17:46:26 +0200

    bruh

--- utility/utils.py
+++ utility/utils.py
@@ -22,7 +22,7 @@ async def disable_all(msg):
 
     for i in range(0, len(new), 5):
         rows.append(ActionRow(*new[i: i + 5]))
-    return new
+    return rows
 
 async def get_bar(health, max_health):
     bar0 = "<:0_:899376245496758343>"
